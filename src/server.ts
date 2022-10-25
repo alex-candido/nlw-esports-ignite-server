ae27eb82575f968d8c7843079dbc832024b1d743
Alex Cândido <alex.candido.tec@gmail.com>
2022-10-25 19:23:03 -0300

    estrutura prisma

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,28 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req, res) => {
+  return res.json([]);
+});
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+  // const gamesId  = req.params.id;
+
   return res.json([
     {id: 1, name: 'Anúncio 1'},
     {id: 2, name: 'Anúncio 2'},
     {id: 3, name: 'Anúncio 3'},
   ])
+})
+
+app.get('/games/:id/discord', (req, res) => {
+  // const addId  = req.params.id;
+
+  return res.json([])
 })
 
 app.listen(4002, () => console.log("Servidor está rodando na porta 4002"))
