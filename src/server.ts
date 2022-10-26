ae824d8155d6b4698056a99ddf23925222d44eff
Alex Cândido <alex.candido.tec@gmail.com>
2022-10-25 23:02:00 -0300

    estrutura final backend

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
 import express from 'express';
 
+import { PrismaClient } from '@prisma/client';
+import { convertHourStringToMinutes } from "./utils/convert-hour-string-to-minutes";
+import { convertMinutesToHourString } from "./utils/convert-minutes-to-hour-string";
+
 const app = express();
+
 app.use(express.json())
+app.use(cors());
 
 const prisma = new PrismaClient({
   log: ['query']
@@ -33,13 +39,13 @@ app.post('/games/:id/ads', async (req, res) => {
       yearsPlaying: body.yearsPlaying,
       discord: body.discord,
       weekDays: body.weekDays.join(','),
-      hourStart: body.hourStart,
-      hourEnd: body.hourEnd,
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
       useVoiceChannel: body.useVoiceChannel,
     },
   })
 
-  return res.status(201).json(body);
+  return res.status(201).json(ad);
 });
 
 app.get('/games/:id/ads', async (req, res) => {
@@ -67,6 +73,8 @@ app.get('/games/:id/ads', async (req, res) => {
     return {
       ...ad,
       weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd),
     }
   }))
 })
